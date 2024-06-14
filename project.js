e855d5d8d6e97ae9fa9feb25858cd3d9fe976a06
Muhammedjashir <Jashirmhd71@gmail.com>
2024-06-14 09:33:38 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,48 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            })
+        }
+    })
+}
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+    
+}
+function sendMail(){
+    var params = {
+        name:document.getElementById("name").value ,
+        email:document.getElementById("email").value ,
+        message:document.getElementById("message").value ,
+    };
+
+const serviceID = "service_j2cpwrr";
+const tamplateID = "template_on8vm75";
+
+emailjs.send(serviceID,tamplateID,params)
+.then(
+    res =>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully")
+    }
+)
+.catch(err=>console.log(err));
+}
